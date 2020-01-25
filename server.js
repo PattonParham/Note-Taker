26c72d0348b16fa9067aa90ce377125341018b48
Patton Parham <patton.parham@verizon.net>
2020-01-24 21:20:01 -0600

    acheived full functionality

--- server.js
+++ server.js
@@ -9,11 +9,8 @@ const PORT = 8000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
-let Notes = [{
-    "title" : "blah",
-    "text" : "blah"}
-    ]
 
 app.get("/", function(req, res){
 res.sendFile(path.join(__dirname, "public/index.html"));
@@ -24,32 +21,79 @@ app.get("/notes", function(req, res){
     });
 
 app.get("/api/notes", function(req, res){
-    return res.json(Notes);
-})    
+    // return res.json(notes);
+    fs.readFile("db/db.json",  function(err, data){
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        return res.json(notes);
+    });
+
+});   
 
 app.listen(PORT, function(){
     console.log("app listening on http://localhost:" + PORT);
 });
 
 app.post("/api/notes", function(req, res){
-    let currentIdNumber = 1
+   
     let newNote = req.body;
-    currentIdNumber = newNote.id;
+    
+    
+    // currentIdNumber = newNote.id;
     console.log(newNote);
-    Notes.push(newNote);
-    currentIdNumber += 1
+   
+    // currentIdNumber += 1
     
     //read the db.json using fs 
-    // fs.readFile("db/db.json", JSON.parse(data), function(err){
-    // })
-    //in that, parse data you get back
-    //let Notearray = JSON.parse(data)
+    fs.readFile("db/db.json",  function(err, data){
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        newNote.id = notes.length + 1;
+            //in that, parse data you get back
+    //let Notearray = array.length-1 
+    
     //array.length-1 
     // newNote.id += 1
     //push newNote into Notearray
+    notes.push(newNote)
     //writeFile("db/db.json", JSON.stringify(newNote), function(err){})
-    fs.appendFile("db/db.json", JSON.stringify(newNote), function(err){
+    
+
+
+
+    fs.writeFile("db/db.json", JSON.stringify(notes), function(err){
         if (err) throw err;
+        return res.json(notes)
         console.log("saved into db.json")
     });
+    });
+
+});
+
+app.delete("/api/notes/:id", function(req, res){
+  
+  fs.readFile("db/db.json",function(err, data){
+      if (err) throw err;
+    let notes = JSON.parse(data);
+    let id = req.params.id;
+    let newNotes = notes.filter(note => note.id != id);
+    for (i = 0; i < newNotes.length; i++){
+        newNotes[i].id = i + 1;
+       
+    }
+    
+    console.log(newNotes)
+    fs.writeFile("db/db.json", JSON.stringify(newNotes), function(err){
+        if (err) throw err;
+        return res.status(202).send();
+        console.log("saved into db.json")
+    });
+    
+      
+
+
+
+
+  });
+       
 });
